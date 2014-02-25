552f264c15f485ba15bacfd283ff1828db55ace8
EDRappaport <ElliRappaport@gmail.com>
2014-02-24 23:35:13 -0500

    might work

--- src/LoadBalancer/ActiveMasters.java
+++ src/LoadBalancer/ActiveMasters.java
@@ -1,7 +1,7 @@
 public class ActiveMasters{
 	private ArrayList<MasterObject> masterList = new ArrayList<MasterObject>();
 
-	public synchronized void putSocket(Socket s) {
+	public void putSocket(Socket s) {
 		PrintWriter pw = new PrintWriter(s.getOutputStream(), true);
 		BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
 		InetAddress ia = s.getInetAddress();
@@ -10,22 +10,31 @@ public class ActiveMasters{
 		masterList.add(MasterObject(s, pw, br, ia, p));
 	}
 
-	private synchronized void removeSocket(int i){
+	private void removeSocket(int i){
 		masterList.remove(i);
 	}
 
-	public synchronized Data requestStats(){
-		int bestScore = 0; int bestIndex = null;
-		for(int i=0; i<masterSocketList.size(); i++){
-			masterWriterList.get(i).println("Give me your stats!");
-			String queueRatio_str = masterReaderList.get(i).readLine();
-			String numProducers_str = masterReaderList.get(i).readLine();
-			String SigarInfo
-			calcScore
-			if calcScore > bestScore{
-				bestScore = calcScore;
-				bestIndex = i;
+	private float calcScore(String queueRatio_str, String numProducers_str, String loadPrevMin_str){
+		return ((1/Float.parseFloat(queueRatio_str)) + (1/Float.parseFloat(loadPrevMin_str)));
+	}
+
+	public synchronized MasterObject requestStats(){
+		float bestScore = 0; int bestIndex = null;
+		for(int i=0; i<masterList.size(); i++){
+			masterList.get(i).pw.println("Give me your stats!");
+			String queueRatio_str = masterList.get(i).br.readLine();
+			String numProducers_str = masterList.get(i).br.readLine();
+			String loadPrevMin_str = masterList.get(i).br.readLine();
+			if (queueRatio_str == null || numProducers_str == null || loadPrevMin_str == null){
+				removeSocket(i);
+			} else{
+				float score = calcScore(queueRatio_str, numProducers_str, loadPrevMin_str);
+				if (calcScore > bestScore) {
+					bestScore = calcScore;
+					bestIndex = i;
+				}
 			}
 		}
+		return masterList.get(i);
 	}
 }
--- src/LoadBalancer/clientSideLB.java
+++ src/LoadBalancer/clientSideLB.java
@@ -4,40 +4,24 @@ import org.hyperic.sigar.*;
 
 public class clientSideLB extends Thread {
 
-	protected static Data getMasterInfo(){
-		private static Sigar sigar = new Sigar();
-		sigar.getLoadAverage()[1];
-		Socket curSocket;
-		int bestScore = 0;
+	public clientSideLB(){
 
-		for(int i=0; i<masterSocketList.size(); i++){
-			curSocket = masterSocketList.get(i);
-			masterWriterList.get(i).println("Give me your stats!");
-			String queueRatio_str = masterReaderList.get(i).readLine();
-			String numProducers_str = masterReaderList.get(i).readLine();
-			if (queueRatio_str == null || numProducers_str == null){
-				masterSocketList.remove(i);
-				masterWriterList.remove(i);
-				masterReaderList.remove(i);
-			}
-			float score = queueRatio_str
-		}
 	}
-	
-	public static void main(String[] args) {
+
+	public void run(){
 
 		try (ServerSocket serverSocket = new ServerSocket(3500)) { 
             while (true) {
                 System.out.println("Listening for Clients");
                 Socket s = serverSocket.accept();
 
-                //Data master = getMasterInfo();
+                MasterObject master = getMasterInfo();
 
                 PrintWriter outToClient = new PrintWriter(s.getOutputStream(), true);
-                //outToClient.println(master.ia.getHostName());
-                //outToClient.println(master.p);
-                outToClient.println("127.0.0.1");
-                outToClient.println("40000");
+                outToClient.println(master.ia.getHostName());
+                outToClient.println(master.p);
+                //outToClient.println("127.0.0.1");
+                ///outToClient.println("40000");
                 s.close();
         
             }
